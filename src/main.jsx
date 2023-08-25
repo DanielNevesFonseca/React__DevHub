be8c66ab5a2564a7b6ace49366e11a84304522d0
Daniel Neves Fonseca - Web Fullstack Developer <daniel.neves.fonseca.ti@gmail.com>
2023-08-25 20:04:17 -0300

    add basic structure of react query

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./providers/UserContext.jsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+// 2° - declarar queryClient e passar como prop de cliente, dentro do componente QueryClientProvider
+const queryClient = new QueryClient();
+
+//1° - envolver todo conteúdo da aplicação com o QueryClientProvider
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
